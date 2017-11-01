a6de86d7cd4c010a31451cc80dda4e89b22eea65
xiaxinyu <summer_west2010@126.com>
2017-11-01 11:04:20 +0800

    init ThreadLocal

---
+++ learning-thread/src/main/java/org/learning/thread/local/Connection.java
@@ -0,0 +1,40 @@
+package org.learning.thread.local;
+
+public class Connection {
+	private static ThreadLocal<Connection> map = new ThreadLocal<Connection>();
+	private String host;
+	private String port;
+
+	private Connection() {
+	}
+
+	public static Connection getConnection() {
+		Connection conn = map.get();
+		if (conn == null) {
+			conn = new Connection();
+			map.set(conn);
+		}
+		return conn;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public String getPort() {
+		return port;
+	}
+
+	public void setPort(String port) {
+		this.port = port;
+	}
+
+	@Override
+	public String toString() {
+		return "Connection [host=" + host + ", port=" + port + "]";
+	}
+}
---
+++ learning-thread/src/main/java/org/learning/thread/local/Test.java
@@ -0,0 +1,48 @@
+package org.learning.thread.local;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class Test {
+	private static Map<Thread, String> threadData = new HashMap<Thread, String>();
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 2; i++) {
+			new Thread() {
+				public void run() {
+					String port = String.valueOf(new Random().nextInt(10000));
+					String host = "Sever" + port;
+
+					Connection.getConnection().setHost(host);
+					Connection.getConnection().setPort(port);
+
+					threadData.put(Thread.currentThread(), host);
+
+					new A().get();
+					new B().get();
+				}
+			}.start();
+		}
+	}
+
+	static class A {
+		public void get() {
+			Thread current = Thread.currentThread();
+			String host = threadData.get(current);
+			System.out.println("A->" + current.getName() + "->" + host);
+			Connection conn = Connection.getConnection();
+			System.out.println("A->Local->" + current.getName() + "->" + conn.getHost() + "->" + conn.getPort());
+		}
+	}
+
+	static class B {
+		public void get() {
+			Thread current = Thread.currentThread();
+			String host = threadData.get(current);
+			System.out.println("B->" + current.getName() + "->" + host);
+			Connection conn = Connection.getConnection();
+			System.out.println("B->Local->" + current.getName() + "->" + conn.getHost() + "->" + conn.getPort());
+		}
+	}
+}
