beb18e0997d55154f76d5c541294bbb08ab65961
xiaxinyu <summer_west2010@126.com>
2017-11-02 15:49:16 +0800

    Optimize SecureCounter
    Need recheck when many threads have write lock in same time

--- learning-thread/src/main/java/org/learning/thread/concurrent/lock/SecureCounter.java
+++ learning-thread/src/main/java/org/learning/thread/concurrent/lock/SecureCounter.java
@@ -1,11 +1,14 @@
 package org.learning.thread.concurrent.lock;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class SecureCounter {
-	private static Counter counter = new Counter();
+	private static int MAX_VALUE = 1000;
+	private static Counter counter = new Counter(MAX_VALUE);
 
 	public static void main(String[] args) {
 		int poolSize = 5;
@@ -14,38 +17,61 @@ public class SecureCounter {
 			pool.execute(new Runnable() {
 				public void run() {
 					while (true) {
-						try {
-							Thread.sleep(50);
-						} catch (InterruptedException e) {
-						}
-
 						int t = counter.getCounter();
-						if (t > 10000) {
+						System.out.println(t + ": " + Thread.currentThread().getName());
+						if (t >= MAX_VALUE) {
+							List<Integer> result = counter.getResult();
+							if (result != null && result.size() > 0) {
+								System.out.println("Counter size: " + result.size());
+							} else {
+								System.out.println("Error occure!");
+							}
 							System.out.println(Thread.currentThread().getName() + " stopped!");
 							break;
 						}
-
-						System.out.println(t + ": " + Thread.currentThread().getName());
 					}
 				}
 			});
 		}
+		pool.shutdown();
 	}
-
 }
 
 class Counter {
 	private int i = 0;
 	private ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 	private int copy = 0;
+	private List<Integer> list = new ArrayList<Integer>();
+	private int max;
+
+	public Counter(int max) {
+		this.max = max;
+	}
 
 	public int getCounter() {
 		lock.readLock().lock();
-		copy = i;
-		lock.readLock().unlock();
-		lock.writeLock().lock();
-		i++;
-		lock.writeLock().unlock();
+		try {
+			copy = i;
+			lock.readLock().unlock();
+			lock.writeLock().lock();
+			try {
+				// Recheck state because another thread might have
+				// current value is greater than max, do nothing
+				if (copy == i && copy < max) {
+					i++;
+					list.add(i);
+				}
+			} finally {
+				lock.writeLock().unlock();
+			}
+			lock.readLock().lock();
+		} finally {
+			lock.readLock().unlock();
+		}
 		return copy;
+	}
+
+	public synchronized List<Integer> getResult() {
+		return list;
 	}
 }
