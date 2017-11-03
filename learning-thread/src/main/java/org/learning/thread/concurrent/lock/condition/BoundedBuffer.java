3a1965aaa6b84fc41f1d5627f5540abebb493174
xiaxinyu <summer_west2010@126.com>
2017-11-03 10:43:24 +0800

    Init Lock.Condition

---
+++ learning-thread/src/main/java/org/learning/thread/concurrent/lock/condition/BoundedBuffer.java
@@ -0,0 +1,60 @@
+package org.learning.thread.concurrent.lock.condition;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BoundedBuffer {
+	private final Lock lock = new ReentrantLock();
+	private final Condition notFull = lock.newCondition();
+	private final Condition notEmpty = lock.newCondition();
+	private final int DEFAULT_CAPACITY = 100;
+	private int capacity = DEFAULT_CAPACITY;
+
+	public BoundedBuffer() {
+
+	}
+
+	public BoundedBuffer(int capacity) {
+		this.capacity = capacity;
+	}
+
+	private Object[] items = new Object[capacity];
+
+	private int putptr, takeptr, count;
+
+	public void put(Object value) throws InterruptedException {
+		lock.lock();
+		try {
+			while (count == capacity) {
+				System.out.println("full !!!!!!!!!!!full!!!!!!!!!!!full!!");
+				notFull.await();
+			}
+			items[putptr] = value;
+			if (++putptr == capacity)
+				putptr = 0;
+			++count;
+			notEmpty.signal();
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public Object take() throws InterruptedException {
+		lock.lock();
+		try {
+			while (count == 0) {
+				System.out.println("hungry !!!!!!!!!hungry!!!!!!hungry!!!!!");
+				notEmpty.await();
+			}
+			Object value = items[takeptr];
+			if (++takeptr == capacity)
+				takeptr = 0;
+			--count;
+			notFull.signal();
+			return value;
+		} finally {
+			lock.unlock();
+		}
+	}
+}
