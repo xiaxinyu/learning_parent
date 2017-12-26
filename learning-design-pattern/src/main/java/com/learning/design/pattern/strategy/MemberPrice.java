6aaec57cb51c6645c89e0a55f5c012a243be8473
xiaxinyu <summer_west2010@126.com>
2017-12-26 10:54:08 +0800

    add strategy pattern

---
+++ learning-design-pattern/src/main/java/com/learning/design/pattern/strategy/MemberPrice.java
@@ -0,0 +1,8 @@
+package com.learning.design.pattern.strategy;
+
+import org.apache.log4j.Logger;
+
+public interface MemberPrice {
+	public Logger logger = Logger.getLogger(MemberPrice.class);
+	public double calcPrice(double price);
+}
---
+++ learning-design-pattern/src/main/java/com/learning/design/pattern/strategy/SilverMemberPrice.java
@@ -0,0 +1,11 @@
+package com.learning.design.pattern.strategy;
+
+public class SilverMemberPrice implements MemberPrice {
+
+	@Override
+	public double calcPrice(double price) {
+		logger.info("Silver members enjoy a 5% discount.");
+		return price * 0.95;
+	}
+
+}
