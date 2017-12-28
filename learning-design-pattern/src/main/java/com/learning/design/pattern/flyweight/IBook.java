89fef4b9793756d6fbc767577c39faaed6d17f70
xiaxinyu <summer_west2010@126.com>
2017-12-28 10:18:12 +0800

    add flyweight pattern

---
+++ learning-design-pattern/src/main/java/com/learning/design/pattern/flyweight/IBook.java
@@ -0,0 +1,11 @@
+package com.learning.design.pattern.flyweight;
+
+public interface IBook {
+	public int getId();
+
+	public void setId(int id);
+
+	public String getName();
+
+	public void setName(String name);
+}
