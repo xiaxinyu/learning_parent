d48a6e90d73cbc6dfc5e6e47df48a699f94b6e8d
xiaxinyu <summer_west2010@126.com>
2017-12-29 10:53:43 +0800

    add iterator pattern

---
+++ learning-design-pattern/src/main/java/com/learning/design/pattern/iterator/Container.java
@@ -0,0 +1,13 @@
+package com.learning.design.pattern.iterator;
+
+public interface Container {
+	public void add(Object obj);
+
+	public void remove(Object obj);
+
+	public Iterator getIterator();
+
+	public int size();
+
+	public Object get(int index);
+}
---
+++ learning-design-pattern/src/main/java/com/learning/design/pattern/iterator/Iterator.java
@@ -0,0 +1,7 @@
+package com.learning.design.pattern.iterator;
+
+public interface Iterator {
+	public boolean hasNext();
+
+	public Object getNext();
+}
