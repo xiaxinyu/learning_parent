92cbec99e9b486cec0351cf11d5cc0c1ecde42b1
xiaxinyu <summer_west2010@126.com>
2018-02-28 10:53:57 +0800

    ThreadPool and single thread work overtime under concurrent

--- learning-thread/src/main/java/org/learning/thread/concurrent/pool/ThreadPoolTest.java
+++ learning-thread/src/main/java/org/learning/thread/concurrent/pool/ThreadPoolTest.java
@@ -2,33 +2,22 @@ package org.learning.thread.concurrent.pool;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class ThreadPoolTest {
 	public static void main(String[] args) {
-		// ExecutorService service = Executors.newFixedThreadPool(3);
+		ExecutorService service = Executors.newFixedThreadPool(3);
 		// ExecutorService service = Executors.newCachedThreadPool();
-		ExecutorService service = Executors.newSingleThreadExecutor();
+		// ExecutorService service = Executors.newSingleThreadExecutor();
 		for (int i = 1; i <= 10; i++) {
 			final int task = i;
 			service.execute(new Runnable() {
 				public void run() {
 					for (int j = 1; j <= 10; j++) {
-						try {
-							Thread.sleep(20);
-						} catch (InterruptedException e) {
-						}     
 						System.out.println(Thread.currentThread().getName() + ":" + j + " is looping in task " + task);
 					}
 				}
 			});
 		}
 		service.shutdownNow();
-		
-		Executors.newScheduledThreadPool(3).scheduleAtFixedRate(new Runnable() {
-			public void run() {
-				System.out.println(Thread.currentThread().getName() + " is excuting task boming");
-			}
-		}, 6, 2, TimeUnit.SECONDS);
 	}
 }
