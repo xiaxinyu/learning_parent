26c8a346f4bbe95e3670b144981465f1c0c27890
xiaxinyu <summer_west2010@126.com>
2018-03-21 17:50:20 +0800

    add learning kafka

---
+++ learning-kafka/src/main/java/com/learning/kafka/core/Constant.java
@@ -0,0 +1,8 @@
+package com.learning.kafka.core;
+
+public class Constant {
+	public static final String HOST_NAME = "192.168.179.131:9092";
+	public static final String TOPIC = "test"; 
+	public static final String CONSUMER_GROUP = "test-consumer-group";
+	public static final int PARTITION = 1;
+}
