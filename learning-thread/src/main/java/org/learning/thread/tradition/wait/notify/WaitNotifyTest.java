5fa98d5013d7017a768187c2bf3af88534ec167c
xiaxinyu <summer_west2010@126.com>
2018-05-11 17:26:50 +0800

    add join

--- learning-thread/src/main/java/org/learning/thread/tradition/wait/notify/WaitNotifyTest.java
+++ learning-thread/src/main/java/org/learning/thread/tradition/wait/notify/WaitNotifyTest.java
@@ -2,31 +2,35 @@ package org.learning.thread.tradition.wait.notify;
 
 public class WaitNotifyTest {
 	public static void main(String[] args) {
-		final Output out = new Output();
-
+		Output op = new Output();
 		new Thread(new Runnable() {
 			public void run() {
-				for (int i = 0; i < 10; i++) {
-					out.printInSubThread();
-				}
+				op.printInSubThread();
 			}
 		}).start();
 
 		new Thread(new Runnable() {
 			public void run() {
-				for (int i = 0; i < 10; i++) {
-					out.printInMainThread();
-				}
+				op.printInMainThread();
 			}
 		}).start();
 	}
 }
 
 class Output {
-	private boolean lockedSub = false;
+	public synchronized void printInMainThread() {
+		for (int i = 0; i < 10; i++) {
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+			}
+			System.out.println("MainThread output " + i);
+		}
+		this.notify();
+	}
 
 	public synchronized void printInSubThread() {
-		if (lockedSub) {
+		if (true) {
 			try {
 				this.wait();
 			} catch (InterruptedException e) {
@@ -39,25 +43,6 @@ class Output {
 			}
 			System.out.println("SubThread output " + i);
 		}
-		lockedSub = true;
-		this.notify();
-	}
-
-	public synchronized void printInMainThread() {
-		if (!lockedSub) {
-			try {
-				this.wait();
-			} catch (InterruptedException e) {
-			}
-		}
-		for (int i = 0; i < 10; i++) {
-			try {
-				Thread.sleep(20);
-			} catch (InterruptedException e) {
-			}
-			System.out.println("MainThread output " + i);
-		}
-		lockedSub = false;
 		this.notify();
 	}
 }
