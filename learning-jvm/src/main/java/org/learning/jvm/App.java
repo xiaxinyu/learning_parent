1383da184669b0747e4360c6c42f0f83dd46f2b0
xiaxinyu <summer_west2010@126.com>
2018-05-15 21:57:48 +0800

    add learning jvm

---
+++ learning-jvm/src/main/java/org/learning/jvm/App.java
@@ -0,0 +1,13 @@
+package org.learning.jvm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
