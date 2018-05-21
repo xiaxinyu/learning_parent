641a28541b8a29f91008c36ed4af1b09e6f59c25
xiaxinyu <summer_west2010@126.com>
2018-05-21 17:37:52 +0800

    update system class loader detection

--- learning-classloader/src/main/java/org/learning/classloader/SystemDetection.java
+++ learning-classloader/src/main/java/org/learning/classloader/SystemDetection.java
@@ -3,5 +3,7 @@ package org.learning.classloader;
 public class SystemDetection {
 	public static void main(String[] args) {
 		System.out.println(System.getProperty("java.class.path"));
+		System.out.println(ClassLoader.getSystemClassLoader().getParent());
+		System.out.println(ClassLoader.getSystemClassLoader().getParent().getParent());
 	}
 }
