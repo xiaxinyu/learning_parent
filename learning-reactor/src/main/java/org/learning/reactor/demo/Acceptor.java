dd28efa2244eeda2c8aa7eeb1e8dca898f4f7436
xiaxinyu <summer_west2010@126.com>
2018-06-07 17:57:51 +0800

    add reactor

---
+++ learning-reactor/src/main/java/org/learning/reactor/demo/Acceptor.java
@@ -0,0 +1,15 @@
+package org.learning.reactor.demo;
+
+public class Acceptor implements Runnable {
+	private Reactor reactor;
+
+	public Acceptor(Reactor reactor) {
+		this.reactor = reactor;
+	}
+
+	@Override
+	public void run() {
+
+	}
+
+}
