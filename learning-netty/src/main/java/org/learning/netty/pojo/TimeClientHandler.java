9b570f9ae7108dc4789f46d51eb85e2c458b2fa6
xiaxinyu <summer_west2010@126.com>
2018-06-10 10:52:42 +0800

    add time to netty

--- learning-netty/src/main/java/org/learning/netty/pojo/TimeClientHandler.java
+++ learning-netty/src/main/java/org/learning/netty/pojo/TimeClientHandler.java
@@ -6,27 +6,13 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 public class TimeClientHandler extends ChannelInboundHandlerAdapter {
 
 	@Override
-	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-		// TODO Auto-generated method stub
-		super.channelReadComplete(ctx);
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		
 	}
 
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-		// TODO Auto-generated method stub
-		super.exceptionCaught(ctx, cause);
+		cause.printStackTrace();
+		ctx.close();
 	}
-
-	@Override
-	public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
-		// TODO Auto-generated method stub
-		super.handlerAdded(ctx);
-	}
-
-	@Override
-	public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
-		// TODO Auto-generated method stub
-		super.handlerRemoved(ctx);
-	}
-
 }
--- learning-netty/src/main/java/org/learning/netty/time/TimeClient.java
+++ learning-netty/src/main/java/org/learning/netty/time/TimeClient.java
@@ -1,4 +1,4 @@
-package org.learning.netty.pojo;
+package org.learning.netty.time;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
@@ -9,17 +9,16 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
-public class PojoClient {
+public class TimeClient {
 	public void start(String host, int port) {
 		EventLoopGroup group = new NioEventLoopGroup();
 		try {
 			Bootstrap bootstrap = new Bootstrap();
 			bootstrap.group(group).channel(NioSocketChannel.class).option(ChannelOption.SO_KEEPALIVE, true)
 					.handler(new ChannelInitializer<SocketChannel>() {
-
 						@Override
 						protected void initChannel(SocketChannel ch) throws Exception {
-							ch.pipeline().addLast(new TimeDecoder(), new TimeClientHandler());
+							ch.pipeline().addLast(new TimeClientHandler());
 						}
 					});
 			ChannelFuture future = bootstrap.connect(host, port).sync();
@@ -32,6 +31,6 @@ public class PojoClient {
 	}
 
 	public static void main(String[] args) {
-		new PojoClient().start("127.0.0.1", 9999);
+		new TimeClient().start("127.0.0.1", 9999);
 	}
 }
