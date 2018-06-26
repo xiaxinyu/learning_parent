0211b7bf52c2d2fff320fe39389f280816bee327
Summer <summer_west2010@126.com>
2018-06-26 22:45:26 +0800

    update hashtable

--- learning-data-structure/src/main/java/org/learning/data/structure/map/HashTableTest.java
+++ learning-data-structure/src/main/java/org/learning/data/structure/map/HashTableTest.java
@@ -2,18 +2,28 @@ package org.learning.data.structure.map;
 
 import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class HashTableTest {
 	public static void main(String[] args) {
-		Hashtable<String, String> tables = new Hashtable<>();
+		Hashtable<String, String> tables = new Hashtable<String, String>();
 		tables.put("summer", "summer1");
 		tables.put("winner", "winner1");
 		tables.put("spring", "spring1");
 		tables.put("autumn", "autumn1");
 
-		Enumeration<String> enums = tables.elements();
-		while(enums.hasMoreElements()) {
+		/*Enumeration<String> enums = tables.elements();
+		while (enums.hasMoreElements()) {
 			System.out.println(enums.nextElement());
+		}*/
+
+		Set<Map.Entry<String, String>> set = tables.entrySet();
+		Iterator<Map.Entry<String, String>> iterator = set.iterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+			iterator.remove();
 		}
 	}
 }
