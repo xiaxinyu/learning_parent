b048b5f765404754654767ef655252cee5d5162e
xiaxinyu <summer_west2010@126.com>
2018-07-02 09:50:53 +0800

    update treeMap

--- learning-data-structure/src/main/java/org/learning/data/structure/map/TreeMapTest.java
+++ learning-data-structure/src/main/java/org/learning/data/structure/map/TreeMapTest.java
@@ -7,17 +7,36 @@ import java.util.Map.Entry;
 
 public class TreeMapTest {
 	public static void main(String[] args) {
-		TreeMap<String, String> treeMap = new TreeMap<String, String>();
+		/*TreeMap<String, String> treeMap = new TreeMap<String, String>();
 		treeMap.put("summer", "oracle");
 		treeMap.put("winner", "java");
 		treeMap.put("autumn", "javascript");
 		treeMap.put("spring", "python");
-		
-		Set<Entry<String, String>> set =treeMap.entrySet();
+
+		Set<Entry<String, String>> set = treeMap.entrySet();
 		Iterator<Entry<String, String>> iterator = set.iterator();
-		while(iterator.hasNext()) {
+		while (iterator.hasNext()) {
 			System.out.println(iterator.next());
 			iterator.remove();
+		}*/
+
+		TreeMap<Integer, String> tree = new TreeMap<Integer, String>();
+		tree.put(12, "summer1");
+		tree.put(8, "summer2");
+		tree.put(9, "summer3");
+		tree.put(88, "summer4");
+		tree.put(100, "summer5");
+		tree.put(2, "summer6");
+		tree.put(7, "summer7");
+		tree.put(6, "summer8");
+		tree.put(34, "summer9");
+		tree.put(67, "summer10");
+
+		Set<Integer> setKey = tree.keySet();
+		Iterator<Integer> iteratorKey = setKey.iterator();
+		while (iteratorKey.hasNext()) {
+			System.out.println(iteratorKey.next());
 		}
+
 	}
 }
