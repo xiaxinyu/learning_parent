64a255e1605eccd405b4c464206b273a0272de68
Summer <summer_west2010@126.com>
2019-01-09 11:46:01 +0800

    Upgrade ribbon

--- learning-spring-cloud-ribbon/src/main/java/org/learning/spring/cloud/ribbon/controller/HelloController.java
+++ learning-spring-cloud-ribbon/src/main/java/org/learning/spring/cloud/ribbon/controller/HelloController.java
@@ -1,6 +1,6 @@
 package org.learning.spring.cloud.ribbon.controller;
 
-import org.learning.spring.cloud.ribbon.register.HelloServiceRegister;
+import org.learning.spring.cloud.ribbon.proxy.SayHelloProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	@Autowired
-	private HelloServiceRegister helloService;
+	private SayHelloProxy proxy;
 	
 	@RequestMapping(value = "sayHello", method = RequestMethod.GET)
 	public String sayHello(@RequestParam String name) {
-		return helloService.hiService(name);
+		return proxy.sayHello(name);
 	}
 }
