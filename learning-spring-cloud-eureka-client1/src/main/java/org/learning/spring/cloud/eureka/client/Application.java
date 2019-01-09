96311389e7a32ed7527e5ef5d841129c552a3e5e
Summer <summer_west2010@126.com>
2019-01-09 10:52:59 +0800

    add eureka client2 for testing

--- learning-spring-cloud-eureka-client1/src/main/java/org/learning/spring/cloud/eureka/client/Application.java
+++ learning-spring-cloud-eureka-client1/src/main/java/org/learning/spring/cloud/eureka/client/Application.java
@@ -12,9 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
-public class App {
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 	@Value("${server.port}")
---
+++ learning-spring-cloud-eureka-client2/src/main/java/org/learning/spring/cloud/eureka/client/Application.java
@@ -0,0 +1,27 @@
+package org.learning.spring.cloud.eureka.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Value("${server.port}")
+	String port;
+
+	@RequestMapping(value = "sayHello", method = RequestMethod.GET)
+	public String sayHello(@RequestParam String name) {
+		return "Hello," + name + ",I am from port:" + port;
+	}
+}
