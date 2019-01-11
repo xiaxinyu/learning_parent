a2737e68e8a9b8fc18a06743fdc148e745d4f7fc
Summer <summer_west2010@126.com>
2019-01-11 15:33:54 +0800

    Upgrade SpringCloud-HystrixDashboard

--- learning-spring-cloud-hystrix-dashboard/src/main/java/org/learning/spring/cloud/hystrix/dashboard/ApplicationHystrixDashboard.java
+++ learning-spring-cloud-hystrix-dashboard/src/main/java/org/learning/spring/cloud/hystrix/dashboard/ApplicationHystrixDashboard.java
@@ -3,15 +3,15 @@ package org.learning.spring.cloud.hystrix.dashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableDiscoveryClient
-@EnableHystrix
 @EnableHystrixDashboard
-public class Application {
+public class ApplicationHystrixDashboard {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ApplicationHystrixDashboard.class, args);
 	}
 }
--- learning-spring-cloud-hystrix-ribbon/src/main/java/org/learning/spring/cloud/hystrix/ApplicationHystrixRibbon.java
+++ learning-spring-cloud-hystrix-ribbon/src/main/java/org/learning/spring/cloud/hystrix/ApplicationHystrixRibbon.java
@@ -2,6 +2,7 @@ package org.learning.spring.cloud.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,9 +11,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableEurekaClient
+@EnableDiscoveryClient
 @EnableHystrix
+@EnableCircuitBreaker 
 public class ApplicationHystrixRibbon {
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationHystrixRibbon.class, args);
